1ae83a01e3254ce224b251e912de94e6924d1d6d
ChenPenelope <164141638+ChenPenelope@users.noreply.github.com>
2025-03-24 10:21:03 +0800

    Update BetHistory.jsx
    delete ({item.multiplier}X)

--- client/src/components/BetHistory.jsx
+++ client/src/components/BetHistory.jsx
@@ -12,7 +12,7 @@ function BetHistory({ history }) {
       <div className='history-list' id='history-list'>
         {[...history].reverse().map((item, index) => (
           <div key={index}>
-            選項{item.option}({item.multiplier}X), 籌碼: {item.amount} &nbsp;
+            選項{item.option}, 籌碼: {item.amount} &nbsp;
             {/*{item.isWin && <span className='result-win'>勝利+{item.amount * item.multiplier}</span>}*/}
             {/*{!item.isWin && <span className='result-lose'>失敗-{item.amount}</span>}*/}
           </div>
