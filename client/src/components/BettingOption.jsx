a7130c4f9fa80eb6634b421d12b22cf8df2523c6
ChenPenelope <164141638+ChenPenelope@users.noreply.github.com>
2025-05-19 21:50:07 +0800

    feat: 新增兩階段投注功能 - 新增投注階段管理 - 修改選項顯示文字 - 添加倒計時功能 - 實現階段切換功能

--- client/src/components/BettingOption.jsx
+++ client/src/components/BettingOption.jsx
@@ -4,15 +4,27 @@ import React from 'react';
 BettingOption.propTypes = {
   index: PropTypes.number.isRequired,
   option: PropTypes.string.isRequired,
+  phase: PropTypes.number.isRequired,
   inputValues: PropTypes.object.isRequired,
   handleInputChange: PropTypes.func.isRequired,
   handleBet: PropTypes.func.isRequired,
 };
 
-function BettingOption({ index, option, inputValues, handleInputChange, handleBet }) {
+function BettingOption({ index, option, phase, inputValues, handleInputChange, handleBet }) {
+  // 根據階段和選項顯示不同的文字
+  const getOptionText = (option, phase) => {
+    if (phase === 1) {
+      // 第一階段的選項
+      return option === 'A' ? '兩份薯不辣一包金餃兩瓶可爾必思，一個草莓口味的哈根達斯還有鐵板麵，蘑菇口味的加蛋' : '兩包薯不辣、一份抓餅、一罐八寶粥、香草口味的哈根達斯、黑胡椒鐵板麵加蛋';
+    } else {
+      // 第二階段的選項
+      return option === 'A' ? '單' : '雙';
+    }
+  };
+
   return (
     <div className='betting-option' key={index}>
-      <div className='option-header'>選項 {option}</div>
+      <div className='option-header'>{getOptionText(option, phase)}</div>
       {/*<p>獎金倍率: {index + 1.5}x</p>*/}
       <div className='bet-controls'>
         <input
@@ -23,7 +35,7 @@ function BettingOption({ index, option, inputValues, handleInputChange, handleBe
           placeholder='投注籌碼'
           onChange={(e) => handleInputChange(index, e.target.value)}
         />
-        <button className='place-bet-button' onClick={() => handleBet(option, index, index)}> 
+        <button className='place-bet-button' onClick={() => handleBet(option, index)}> 
         {/* <button className='place-bet-button' onClick={() => handleBet(option, index + 1.5, index)}>*/}
           投注
         </button>
