6430b568d42f32778690844e8adc11f1bd88af88
ChenPenelope <164141638+ChenPenelope@users.noreply.github.com>
2025-05-28 11:46:43 +0800

    修復：恢復 vite 配置並調整依賴安裝

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
